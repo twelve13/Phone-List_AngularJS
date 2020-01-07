4d5d2405294c2175fb59fe185cfc0c4c38861135
Caroline Chen <carolinecaroline@gmail.com>
2020-01-07 17:00:55 -0500

    display entries, make searchable

---
+++ app.js
@@ -0,0 +1,460 @@
+var individualsList = [
+	{
+		section: "staff",
+		name: "Faculty",
+		id: "faculty",
+		members: [
+			{
+				name: "Bathsheda Babbling",
+				title: "Professor, Ancient Runes",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Cuthbert Binns",
+				title: "Professor, History of Magic",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Phineas Nigellus Black",
+				title: "(Formerly) Headmaster",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Amelia Bones",
+				title: "Head, Dept of Law Enforcement",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Charity Burbage",
+				title: "Professor, Muggle Studies",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Alecto Carrow",
+				title: "Professor, Muggle Studies",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Amycus Carrow",
+				title: "Professor, Dark Arts",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Elphias Doge",
+				title: "Special Advisor",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Albus Dumbledore",
+				title: "Headmaster",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Filius Flitwick",
+				title: "Professor, Charms",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Gilderoy Lockhart",
+				title: "Professor, Defense",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Remus Lupin",
+				title: "Professor, Defense",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			}
+		]
+	},
+	{
+		section: "staff",
+		name: "Other Staff",
+		id: "other-staff",
+		members: [
+			{
+				name: "Argus Filch",
+				title: "Caretaker",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Godric Gryffindor",
+				title: "Founder",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Rubeus Hagrid",
+				title: "Groundkeeper",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Rolanda Hooch",
+				title: "Referee",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Helga Hufflepuff",
+				title: "Founder",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Irma Prince",
+				title: "Librarian",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Rowena Ravenclaw",
+				title: "Founder",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Salazar Slytherin",
+				title: "Founder",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			}
+		]
+	},
+	{
+		section: "staff",
+		name: "Ministry",
+		id: "ministry",
+		members: [
+			{
+				name: "Ludo Bagman",
+				title: "Head, Games and Sports",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Cornelius Fudge",
+				title: "Minister",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			}
+		]
+	},
+	{
+		section: "associates",
+		name: "Associates",
+		id: "associates",
+		members: [
+			{
+				name: "Bathilda Bagshot",
+				title: "Author",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Frank Bryce",
+				title: "Gardener",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Mundungus Fletcher",
+				title: "Dealer of Artifacts",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Xenophilius Lovegood",
+				title: "Editor",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Madam Malkin",
+				title: "Shop Owner",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Poppy Pomfrey",
+				title: "Matron, Hospital Wing",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Ernest Prang",
+				title: "Driver",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Newton Scamander",
+				title: "Author",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			}
+		]
+	},
+	{
+		section: "students",
+		name: "Gryffindor",
+		id: "gryffindor",
+		colors: "red and gold",
+		mascot: "lion",
+		characteristics: "daring and brave",
+		members: [
+			{
+				name: "Katie Bell",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Lavender Brown",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Colin Creevey",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Dennis Creevey",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Seamus Finnigan",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Hermione Granger",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Angelina Johnson",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Lee Jordan",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Neville Longbottom",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Harry Potter",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Ginevra Weasley",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Ronald Weasley",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Oliver Wood",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			}
+		]
+	},
+	{
+		section: "students",
+		name: "Slytherin",
+		id: "slytherin",
+		colors: "green and silver",
+		mascot: "serpent",
+		characteristics: "ambitious and cunning",
+		members: [
+			{
+				name: "Millicent Bulstrode",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Vincent Crabbe",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Marcus Flint",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Gregory Goyle",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Draco Malfoy",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Theodore Nott",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Pansy Parkinson",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			}
+		]
+	},
+	{
+		section: "students",
+		name: "Ravenclaw",
+		id: "ravenclaw",
+		colors: "blue and bronze",
+		mascot: "eagle",
+		characteristics: "wise",
+		members: [
+			{
+				name: "Terry Boot",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Cho Chang",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Penelope Clearwater",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Michael Corner",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Marietta Edgecombe",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Anthony Goldstein",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Luna Lovegood",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Padma Patil",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			}
+		]
+	},
+	{
+		section: "students",
+		name: "Hufflepuff",
+		id: "hufflepuff",
+		colors: "yellow and black",
+		mascot: "badger",
+		characteristics: "loyal and fair",
+		members: [
+			{
+				name: "Hannah Abbott",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Susan Bones",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Ernest Macmillan",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			},
+			{
+				name: "Zacharias Smith",
+				officeNumber: "123-456-7890",
+				cellNumber: "098-765-4321"
+			}
+		]
+	}
+];
+
+angular
+	.module("phonelistApp", [])
+	.controller("phonelistController", ["$scope", "$filter", phonelistControllerFunction])
+	.controller("ScrollController", ["$scope", "$location", "$anchorScroll", scrollControllerFunction])
+
+	function phonelistControllerFunction ($scope, $filter) {
+		$scope.allIndividuals = individualsList;
+		$scope.staff = $filter("filter")(individualsList, {section: "staff"}, true);
+		$scope.students = $filter("filter")(individualsList, {section: "students"}, true);
+	}
+
+	function scrollControllerFunction($scope, $location, $anchorScroll) {
+		var scrollMore = -190;
+		var scrollLess = -150;
+
+		$scope.gotoTop = function() {
+			$location.hash("top");
+			$anchorScroll();
+		}
+
+		$scope.scrollToSection = function() {
+			document.getElementById(this.section.id).scrollIntoView(true);
+			if (window.innerWidth > 768) {
+				window.scrollBy(0, scrollLess);
+			}
+			else {
+				window.scrollBy(0, scrollMore);
+			}
+		}
+
+		$scope.scrollStudents = function() {
+			document.getElementById("students").scrollIntoView(true);
+			if(window.innerWidth > 768) {
+				window.scrollBy(0, scrollLess);
+			}
+			else {
+				window.scrollBy(0, scrollMore);
+			}
+		}
+	};
+
+	const hamburgerButton = document.querySelector("#hamburger");
+	const agenciesList = document.querySelector(".agencies");
+
+	const toggleList = function() {
+		agenciesList.classList.toggle("show-list")
+	}
+
+	hamburgerButton.addEventListener("click", toggleList);
